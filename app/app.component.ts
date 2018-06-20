63aa311c70ad8f23e2ea386d056de935aff1e9e5
Giuseppe Rota <rota.giuseppe@gmail.com>
2018-06-20 19:03:39 +0200

    ngIf and web components (templates)

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,25 @@ import {Component} from '@angular/core'
   selector: 'app-root',
   template: `
   <div class="app">
-  <button (click)="handleClick(username.value)" >get value</button>
-    <input type="text" #username>
-    <div> {{ name }} </div>
+    <input type="text"
+    [value]="name"
+    (input)=handleChange($event.target.value)
+    >
   </div>
+  <div *ngIf="name.length > 2">
+    Searching for... {{name}}
+  </div>
+  <template [ngIf]="name.length > 2">
+    <div>
+      TPL Searching for... {{name}}
+    </div>
+  </template>
   `,
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  name: string = 'todd';
-  handleClick(v: string) {
-    console.log(v);
+  name: string = '';
+  handleChange(value: string) {
+    this.name=value;
   }
 }
