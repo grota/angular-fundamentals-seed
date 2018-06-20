696d94da0460395abbfdbbe334bbe0fd5745fda7
Giuseppe Rota <rota.giuseppe@gmail.com>
2018-06-20 18:22:18 +0200

    single way data binding and prop binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+  <div class="app">
+  <button 
+  (click)="handleClick()"
+  >press me </button>
+    <input
+    type="text"
+    [value]="name"
+    (blur)="handleBlur($event)"
+    (input)="handleInput($event)"
+    >
+    {{ name }}
+  </div>
+  `,
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-
+  name: string = 'todd';
+  handleInput(event: any) {
+    this.name = event.target.value;
+  };
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
+  }
+  handleClick() {
+    this.name = 'aaaaaaa';
+  }
 }
