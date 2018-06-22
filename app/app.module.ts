7162f903cf2c625c485696291011a6ff63620e24
Giuseppe Rota <rota.giuseppe@gmail.com>
2018-06-22 13:17:07 +0200

    additional custom module+component+interfaces

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { CommonModule } from '@angular/common'
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module'
 
 import { AppComponent } from './app.component'
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component'
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    PassengerDashboardModule
   ],
   bootstrap: [AppComponent]
 })
