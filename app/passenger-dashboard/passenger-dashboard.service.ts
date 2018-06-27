028811089fd3819a9b67ceab662dc01d970695a4
Giuseppe Rota <rota.giuseppe@gmail.com>
2018-06-27 12:19:24 +0200

    Observable.catch error handling

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 const PASSENGER_API: string = '/api/passengers';
 
@@ -15,16 +17,19 @@ export class PassengerDashboardService {
     return this.http
     .put(`${PASSENGER_API}/${passenger.id}`, passenger)
     .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()))
   }
 
   removePassenger(passenger: Passenger): Observable<Passenger> {
     return this.http
     .delete(`${PASSENGER_API}/${passenger.id}`)
     .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()))
   }
   getPassengers(): Observable<Passenger[]> {
     return this.http
     .get(PASSENGER_API)
     .map((response: Response) => response.json())
+    .catch((error: any) => Observable.throw(error.json()))
   }
 }
