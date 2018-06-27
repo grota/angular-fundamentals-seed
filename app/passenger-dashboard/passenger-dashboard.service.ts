743960c2f32d3d8383050a8b23d714f86c79f4c1
Giuseppe Rota <rota.giuseppe@gmail.com>
2018-06-27 11:38:17 +0200

    update+delete hitting the apis that touch the db.json

--- app/passenger-dashboard/passenger-dashboard.service.ts
+++ app/passenger-dashboard/passenger-dashboard.service.ts
@@ -11,6 +11,17 @@ export class PassengerDashboardService {
 
   constructor(private http: Http) {}
 
+  updatePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+    .put(`${PASSENGER_API}/${passenger.id}`, passenger)
+    .map((response: Response) => response.json())
+  }
+
+  removePassenger(passenger: Passenger): Observable<Passenger> {
+    return this.http
+    .delete(`${PASSENGER_API}/${passenger.id}`)
+    .map((response: Response) => response.json())
+  }
   getPassengers(): Observable<Passenger[]> {
     return this.http
     .get(PASSENGER_API)
